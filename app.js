cf6c18e778755787bf83eff402d753cb70222c26
Sara Khan Tareen <sara.tareen@gmail.com>
2023-07-03 14:46:35 +0500

    - Connected it to a server

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const amount = 12
+const http = require('http')
+const { readFileSync } = require('fs')
 
-if(amount <10){
-    console.log('small');
-}
-else{
-    console.log('large')
-}
+const homePage = readFileSync('./homepage.html')
+const homeCss = readFileSync('./homepage.css')
 
-console.log('node')
+const server = http.createServer((req,res) => {
+    const url = req.url;
+    if (url === '/'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+     else if (url === '/homepage.css') {
+        res.writeHead(200, {'content-type': 'text/css'});
+        res.write(homeCss);
+        res.end();
+    }
+    else if (url === '/history'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>History</h1>')
+        res.end()
+    }
+    else {
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
+    
+})
+
+server.listen(5000)
